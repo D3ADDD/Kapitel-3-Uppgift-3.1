9df0a51d553334d9a360e73926f85f6fe4107339
D3ADDD <95422574+D3ADDD@users.noreply.github.com>
2025-11-28 08:21:13 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int age = int.Parse(Console.ReadLine());
+            if (age > 15 && age < 20)
+            {
+                Console.WriteLine("Du får delta i tävlingen!");
+            }
+            else
+            {
+                Console.WriteLine("Tyvärr, du får inte delta i tävlingen.");
+            }
+
+        }
+
+    }
+}
